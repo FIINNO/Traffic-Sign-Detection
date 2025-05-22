10b826bc8415bd4071d3e965700a1ee7c31cfafe
Alfbom27 <alst2104@student.miun.se>
2025-05-22 09:49:02 +0200

    update

--- evaluate.py
+++ evaluate.py
@@ -1,54 +1,132 @@
 from ultralytics.utils.ops    import non_max_suppression, xyxy2xywh, xywh2xyxy
 from ultralytics.utils.metrics import bbox_iou
 import torch
-
-def evaluate():
-    model.eval()
-    all_preds = []
-    all_gts = []
-    validation_loop = tqdm(val_loader, desc=f"Training epoch: {epoch+1}/{epochs}")
-    for batch in validation_loop:
-        batch = {k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in batch.items()}
-        images = batch["img"]
-        gt_boxes = batch["bboxes"]
-        gt_classes = batch["cls"]
-
-        with torch.no_grad():
-            output = model(images)
-
-        nms_results = non_max_suppression(
-            output,
-            conf_thres=0.25,
-            iou_thres=0.45,
-        )
-        print("NMS_RESULTS: ", nms_results)
-
-        for i, pred in enumerate(nms_results):
-            pred_boxes = pred[:, :4]
-            pred_conf = pred[:, 4]
-            pred_classes = pred[:, 5]
-
-            pred_boxes_xywh = xyxy2xywh(pred_boxes)
-            img_size = 640
-            pred_boxes_xywh[:, 0] /= img_size
-            pred_boxes_xywh[:, 1] /= img_size
-            pred_boxes_xywh[:, 2] /= img_size
-            pred_boxes_xywh[:, 3] /= img_size
-
-            true_boxes = gt_boxes[i]
-            true_classes = gt_classes[i]
-
-            iou = bbox_iou(pred_boxes_xywh, true_boxes)
-            #print("IOU", iou)
-
-            all_preds.append((pred_boxes_xywh, pred_conf, pred_classes))
-            all_gts.append((true_boxes, true_classes))
-    #print(all_preds)
-    #print(all_gts)
-    return images, all_gts, all_preds
-
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+
+
+
+
+
+def process_batches(batch_list):
+    """
+    Extracts data instances from each batch into a list.
+    Args:
+        batch_list (List[batch]): List of batches containing ground truths
+
+    Returns:
+        (List[dict]): List of dicts containing object ground truths for each dataset instance.
+    """
+
+    batch_size = batch_list[0]['img'].shape[0]
+    labels = []
+    for i in range(len(batch_list)):
+        for img_index in range(batch_size):
+            mask = batch_list[i]['batch_idx'] == img_index
+            image_labels = {
+               'bboxes': batch_list[i]['bboxes'][mask],
+                'cls': batch_list[i]['cls'][mask],
+            }
+            labels.append(image_labels)
+
+    return labels
+
+
+def compute_metrics(preds, batch_list, iou_threshold=0.5, img_size=640):
+    """
+    Computes recall and precision of model predictions
+    Args:
+        preds (List[tensor]): List of model predictions
+        batch_list (List[batch]): List of batches containing ground truths
+        iou_threshold (float): IoU threshold
+        img_size (int): Image size
+
+    Returns:
+
+    """
+    labels = process_batches(batch_list)
+
+    tp = 0
+    fp = 0
+    total_objects = 0
+
+    # Loop through prediction and gt for each data instance
+    for pred, gt in zip(preds, labels):
+
+        nms_results = non_max_suppression(  # nms
+            pred,
+            conf_thres=0.25,
+            iou_thres=0.5,
+            max_det=1000
+        )
+
+        pred_boxes = nms_results[0][:,:4]
+        pred_conf = nms_results[0][:,4]
+        pred_classes = nms_results[0][:,5]
+
+        # Convert bboxes from x1y1x2y2 to xywh and normalize
+        pred_boxes_xywh = xyxy2xywh(pred_boxes)
+        pred_boxes_xywh[:, 0] /= img_size
+        pred_boxes_xywh[:, 1] /= img_size
+        pred_boxes_xywh[:, 2] /= img_size
+        pred_boxes_xywh[:, 3] /= img_size
+
+
+        gt_boxes = gt['bboxes']
+        gt_classes = gt['cls']
+        total_objects += len(gt_boxes)
+
+        matched = []
+        for pred_box, pred_class in zip(pred_boxes_xywh, pred_classes):
+
+            match_found = False
+            for i in range(len(gt_boxes)):
+                if i in matched:
+                    continue
+                if pred_class != gt_classes[i]:
+                    continue
+                iou = bbox_iou(pred_box, gt_boxes[i])
+                if iou >= iou_threshold:
+                    matched.append(i)
+                    match_found = True
+                    break
+            if match_found:
+                tp += 1
+            else:
+                fp += 1
+
+
+    fn = total_objects - tp
+    print(f"Total tp: {tp}, fp: {fp}, fn: {fn}")
+    precision = tp / (tp + fp + 1e-9)
+    recall = tp / (total_objects + 1e-9)
+    print(f"Precision: {precision}, Recall: {recall}")
+
+
+
+
+
+def evaluate_yolo(model, test_loader):
+    # evaluate on test dataset
+    total_gt = []
+    total_preds = []
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    model.eval()
+    with torch.no_grad():
+        for batch in test_loader:
+            total_gt.append(batch)
+            batch = {k: v.to(device) if isinstance(v, torch.Tensor) else v for k, v in batch.items()}
+
+            images = batch['img']
+            output = model.forward(images)
+
+            for i in range(output[0].size(0)):
+                total_preds.append(output[0][i].unsqueeze(0).cpu())
+
+    compute_metrics(total_preds, total_gt)
+
 
 
 def visualize_pred_vs_gt(img, gt, pred):
