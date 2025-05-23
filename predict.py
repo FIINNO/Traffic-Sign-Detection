e176e4cec345bfd021d54b45de5947cb575925b8
Alfbom27 <alst2104@student.miun.se>
2025-05-23 14:02:42 +0200

    update

---
+++ predict.py
@@ -0,0 +1,50 @@
+import torch
+from letterbox import letterbox
+import cv2
+from ultralytics.utils.ops import non_max_suppression
+from evaluate import visualize_pred_vs_gt
+from ultralytics.nn.tasks import DetectionModel
+
+
+
+def predict(img, model):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_padded, _ = letterbox(img)
+    img_tensor = torch.from_numpy(img_padded).permute(2, 0, 1).float() / 255.0
+    assert img_tensor.shape == (3, 640, 640), f"Got {img_tensor.shape}"
+    img_tensor = img_tensor.unsqueeze(0)
+    print(img_tensor.shape)
+
+    model.eval()
+    with torch.no_grad():
+        output = model.forward(img_tensor)
+        nms_results = non_max_suppression(
+            output[0],
+            conf_thres=0.1,
+            iou_thres=0.5,
+            max_det=1000
+        )
+        print(nms_results)
+
+
+    visualize_pred_vs_gt(img=img_tensor[0], nms=nms_results)
+
+
+
+
+checkpoint = torch.load('yolo11l.pt')
+cfg = checkpoint['model'].yaml
+
+model = DetectionModel(cfg=cfg, nc=243, ch=3, verbose=True)
+
+model.load_state_dict(torch.load('yolol_checkpoint_best.pth', map_location=lambda storage, loc: storage))
+
+img = cv2.imread('./img_6.png')
+predict(img, model)
+
+
+
+
+
+
+
